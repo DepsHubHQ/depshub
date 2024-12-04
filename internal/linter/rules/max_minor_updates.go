417bfed8f9c2144b8c5cc32bbee0146b3387b93e
Andriy Semenets <semanser@gmail.com>
2024-12-04 10:52:07 +0100

    Add a new max-minor-updates rule

---
+++ internal/linter/rules/max_minor_updates.go
@@ -0,0 +1,68 @@
+package rules
+
+import (
+	"github.com/depshubhq/depshub/pkg/types"
+)
+
+const MaxMinorUpdatesPercent = 60.0
+
+type RuleMaxMinorUpdates struct {
+	name  string
+	level Level
+}
+
+func NewRuleMaxMinorUpdates() RuleMaxMinorUpdates {
+	return RuleMaxMinorUpdates{
+		name:  "max-minor-updates",
+		level: LevelError,
+	}
+}
+
+func (r RuleMaxMinorUpdates) GetMessage() string {
+	return "The total number of minor updates is too high"
+}
+
+func (r RuleMaxMinorUpdates) GetName() string {
+	return r.name
+}
+
+func (r RuleMaxMinorUpdates) GetLevel() Level {
+	return r.level
+}
+
+func (r RuleMaxMinorUpdates) Check(manifests []types.Manifest, info PackagesInfo) ([]Mistake, error) {
+	mistakes := []Mistake{}
+	definitions := []types.Definition{}
+	totalDependencies := 0
+
+	for _, manifest := range manifests {
+		for _, dep := range manifest.Dependencies {
+			if pkg, ok := info[dep.Name]; ok {
+				totalDependencies++
+				major, minor, patch := parseVersion(dep.Version)
+
+				for v := range pkg.Versions {
+					ma, mi, p := parseVersion(v)
+
+					if mi > minor && ma == major && p == patch {
+						definitions = append(definitions, dep.Definition)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	if totalDependencies == 0 {
+		return mistakes, nil
+	}
+
+	if float64(len(definitions))/float64(totalDependencies)*100 > MaxMinorUpdatesPercent {
+		mistakes = append(mistakes, Mistake{
+			Rule:        r,
+			Definitions: definitions,
+		})
+	}
+
+	return mistakes, nil
+}
